b31e3130f3707b98450baa58dd96d7cedc96d9a9
Tyler Afflerbach <TAfflerbach@sorenson.com>
2019-10-25 16:23:48 -0600

    Add project files.

---
+++ twenty one bugs/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace twenty_one_bugs.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
